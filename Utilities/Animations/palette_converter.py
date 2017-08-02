f5aa5a41f1dfbc143559070abdbe8fd79aaaa041
rainlash <rain.pipe.ash@gmail.com>
2017-08-02 18:35:18 -0500

    Adding support for Unarmed Animations

--- Utilities/Animations/palette_converter.py
+++ Utilities/Animations/palette_converter.py
@@ -3,9 +3,9 @@ import glob
 from palette_index import *
 
 ### === INPUTS ===
-p1 = mage_red
-p2 = mage_blue
-images_to_convert = 'Mage-MagicAnima-GenericRed.png'
+p1 = myrmidon_red
+p2 = myrmidon_blue
+images_to_convert = '*.png'
 
 def palette_convert(p1, p2, image_to_convert):
 	conversion = {p1[i]: p2[i] for i in xrange(len(p1))}
