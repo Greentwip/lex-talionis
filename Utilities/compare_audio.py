54ceb9b0ae4035312aa5ac4af2e23e8cb0710e26
rainlash <rainlash>
2017-09-24 14:56:00 -0500

    compare audio

--- Utilities/compare_audio.py
+++ Utilities/compare_audio.py
@@ -1,13 +1,23 @@
 #! /usr/bin/env python2.7
 
 # Next step is to given a large file, split it at silences and test each subsound
-import glob
+import glob, time, sys
 from pydub import AudioSegment
+import pygame
 
-MILLISECONDS = 3
+pygame.mixer.pre_init(44100, -16, 2, 4096)
+pygame.init()
+pygame.mixer.init()
+
+pygame.display.set_mode((240, 160))
+
+# Nominally 3
+MILLISECONDS = 1
 NOISE_FLOOR = 0.08
 FOLDER = 'FE_Sounds/'
-TEST_ME = 'save.wav'
+TEST_ME = 'CombatDeath.wav'
+
+SFXDICT = {sfx: pygame.mixer.Sound(sfx) for sfx in glob.glob(FOLDER + '*.wav')}
 
 def parse_song(song):
     length = len(song)
@@ -32,6 +42,7 @@ def parse_song(song):
     song_facts['length'] = length
     song_facts['max_db'] = max_db
     song_facts['cascade'] = cascade
+    song_facts['song'] = song
     return song_facts
 
 # Create database
@@ -48,6 +59,7 @@ for fp, song in database:
     song['cascade'] = [c/absolute_max for c in song['cascade']]
 
 to_test = AudioSegment.from_wav(TEST_ME)
+my_song = pygame.mixer.Sound(TEST_ME)
 print('Processing %s' % to_test)
 print(to_test.frame_rate)
 # # print(len(to_test))
@@ -58,21 +70,42 @@ to_test_values = parse_song(to_test)
 to_test_values['cascade'] = [c/absolute_max for c in to_test_values['cascade']]
 
 # Comparison
-matches = []
 for fp, song in database:
     # print(fp, song['length'], to_test_values['length'])
     # Don't worry about songs with drastically different lengths
     if abs(song['length'] - to_test_values['length']) > 1000:
+        song['comp'] = 100
         continue
-    if abs(len(song['cascade']) - len(to_test_values['cascade'])) > 50:
+    if abs(len(song['cascade']) - len(to_test_values['cascade'])) > 200:
+        song['comp'] = 100
         continue
     comp = sum([abs(i - j) for i, j in zip(song['cascade'], to_test_values['cascade'])])/float(len(to_test_values['cascade']))
+    song['comp'] = comp
     # print(comp)
-    if abs(comp) < 0.02:
-        print(fp, len(song['cascade']), len(to_test_values['cascade']))
-        matches.append((fp, abs(comp)))
+    # if abs(comp) < 0.02:
+    #     print(fp, len(song['cascade']), len(to_test_values['cascade']))
+    #     matches.append((fp, abs(comp)))
+
+def terminate():
+    pygame.mixer.music.stop()
+    pygame.mixer.quit()
+    pygame.quit()
+    sys.exit()
+
+def listen():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                terminate()
 
 print('Matches:')
-for fp, confidence in sorted(matches, key=lambda x: x[1]):
-    print('%s: %s' %(fp, confidence))
+for fp, song in sorted(database, key=lambda x: x[1]['comp'])[:50]:
+    listen()
+    # my_song.play()
+    # time.sleep(my_song.get_length() + 0.2)
+    print('%s: %s' %(fp, song['comp']))
+    SFXDICT[fp].play()
+    time.sleep(SFXDICT[fp].get_length() + 0.2)
 print('Done!')
