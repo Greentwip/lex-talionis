cc71ce589c19384960c2fe7344b610d9805b5ad9
rainlash <rain.pipe.ash@gmail.com>
2017-10-24 19:11:37 -0500

    Cleric Sprite

--- Utilities/Animations/palette_converter.py
+++ Utilities/Animations/palette_converter.py
@@ -3,8 +3,8 @@ import glob
 from palette_index import *
 
 ### === INPUTS ===
-p1 = karla
-p2 = marisa
+p1 = serra
+p2 = tamiko
 images_to_convert = '*.png'
 
 def palette_convert(p1, p2, image_to_convert):
@@ -23,7 +23,7 @@ def palette_convert(p1, p2, image_to_convert):
                         continue
                     image.putpixel((x, y), new_color)
 
-    image.save('fixed_' + image_to_convert)
+    image.save(image_to_convert)
 
 for fp in glob.glob(images_to_convert):
     print(fp)
