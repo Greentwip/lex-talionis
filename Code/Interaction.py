62be9dd57ca5829150fa491e80a38cfc796d5b86
rainlash <rain.pipe.ash@gmail.com>
2018-02-01 18:18:10 -0600

    Fixing bug with half_lifelink and interaction between AOE and spells

--- Code/Interaction.py
+++ Code/Interaction.py
@@ -86,7 +86,7 @@ class Solver(object):
 
         # if cf.OPTIONS['debug']: print('To Hit:', to_hit, ' Roll:', roll)
         if self.item.weapon:
-            if roll < to_hit and not (defender in self.splash and (isinstance(defender, TileObject.TileObject) or 'evasion' in defender.status_bundle)):
+            if roll < to_hit and (defender not in self.splash or 'evasion' not in defender.status_bundle):
                 result.outcome = (2 if self.item.guaranteed_crit else 1)
                 result.def_damage = self.attacker.compute_damage(defender, gameStateObj, self.item, mode='Attack', hybrid=hybrid)
                 if cf.CONSTANTS['crit']: 
@@ -98,7 +98,7 @@ class Solver(object):
                 # print(result.def_damage)
 
         elif self.item.spell:
-            if not self.item.hit or roll < to_hit:
+            if not self.item.hit or (roll < to_hit and (defender not in self.splash or 'evasion' not in defender.status_bundle)):
                 result.outcome = (2 if self.item.guaranteed_crit else 1)
                 if self.item.damage is not None:
                     result.def_damage = self.attacker.compute_damage(defender, gameStateObj, self.item, mode='Attack', hybrid=hybrid)
@@ -169,6 +169,8 @@ class Solver(object):
         if result.def_damage > 0:
             if self.defender.getMainWeapon().lifelink:
                 result.atk_damage -= result.def_damage
+            if self.defender.getMainWeapon().half_lifelink:
+                result.atk_damage -= result.def_damage/2
             # Handle Vampire Status
             for status in self.defender.status_effects:
                 if status.vampire and self.attacker.currenthp - result.def_damage <= 0 and \
