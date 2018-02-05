4be03c34669378d513ffe77eb19db38ff2b86424
rainlash <rainlash>
2018-02-04 18:02:49 -0600

    Made Custom Sentinel MapSprite

--- Utilities/enemy_palette_swap.py
+++ Utilities/enemy_palette_swap.py
@@ -4,36 +4,41 @@
 from PIL import Image
 import os
 
-PaletteDict = {(56,56,144):(96,40,32),
-               (57,57,148):(96,40,32),
-               (56,80,224):(168,48,40),
-               (57,82,230):(168,48,40),
-               (40,160,248):(224,16,16),
-               (24,240,248):(248,80,72),
-               (232,16,24):(56,208,48),
-               (88,72,120):(104,72,96),
-               (144,184,232):(192,168,184)}
+PaletteDict = {(56, 56, 144): (96, 40, 32),
+               (56, 80, 224): (168, 48, 40),
+               (40, 160, 248): (224, 16, 16),
+               (24, 240, 248): (248, 80, 72),
+               (232, 16, 24): (56, 208, 48),
+               (88, 72, 120): (104, 72, 96),
+               (64, 56, 56): (64, 56, 56),
+               (216, 232, 240): (224, 224, 224),
+               (248, 248, 64): (248, 248, 64),
+               (112, 96, 96): (112, 96, 96),
+               (64, 56, 56): (64, 56, 56),
+               (248, 248, 248): (248, 248, 248),
+               (176, 144, 88): (176, 144, 88),
+               (248, 248, 208): (248, 248, 208),
+               (128, 136, 112): (128, 136, 112),
+               (144, 184, 232): (192, 168, 184)}
 
 for fp in os.listdir('.'):
-  if fp.endswith('.png'):
-    picture = Image.open(fp)
+    if fp.endswith('.png'):
+        picture = Image.open(fp)
 
-    # Get the size of the image
-    width, height = picture.size
+        # Get the size of the image
+        width, height = picture.size
 
-    newimg = Image.new("RGB", [width, height], (255,255,255))
-    # Change color
-    for x in range(width):
-        for y in range(height):
-            current_color = picture.getpixel((x,y))
-            try:
-                new_color = PaletteDict[current_color]
-            except KeyError:
-                new_color = current_color
-            newimg.putpixel((x,y), new_color)
+        newimg = Image.new("RGB", (width, height), (128, 160, 128))
+        # Change color
+        for x in range(width):
+            for y in range(height):
+                current_color = picture.getpixel((x, y))
+                try:
+                    new_color = PaletteDict[current_color]
+                except KeyError:
+                    if current_color != (128, 160, 128):
+                        print(current_color)
+                    new_color = current_color
+                newimg.putpixel((x, y), new_color)
 
-    newimg.save("enemy" + str(fp[6:]))
-
-
-#newimg.show()
-        
+        newimg.save("enemy" + str(fp[6:]))        
--- Utilities/passive_map_sprite_creator.py
+++ Utilities/passive_map_sprite_creator.py
@@ -4,49 +4,50 @@
 from PIL import Image
 import os
 
-PaletteDict = {(88,72,120):(64,64,64),
-               (144,184,232):(120,120,120),
-               (216,232,240):(184,184,184),
-               (112,96,96):(80,80,80),
-               (176,144,88):(128,128,128),
-               (248,248,208):(200,200,200),
-               (56,56,144):(72,72,72),
-               (56,80,224):(88,88,88),
-               (40,160,248):(152,152,152),
-               (24,240,248):(184,184,184),
-               (232,16,24):(112,112,112),
-               (248,248,64):(200,200,200),
-               (248,248,248):(208,208,208)}
+PaletteDict = {(88, 72, 120): (64, 64, 64),
+               (144, 184, 232): (120, 120, 120),
+               (216, 232, 240): (184, 184, 184),
+               (112, 96, 96): (80, 80, 80),
+               (176, 144, 88): (128, 128, 128),
+               (248, 248, 208): (200, 200, 200),
+               (56, 56, 144): (72, 72, 72),
+               (56, 80, 224): (88, 88, 88),
+               (40, 160, 248): (152, 152, 152),
+               (24, 240, 248): (184, 184, 184),
+               (232, 16, 24): (112, 112, 112),
+               (248, 248, 64): (200, 200, 200),
+               (248, 248, 248): (208, 208, 208)}
 
 def grayconvert(newimg):
     width, height = newimg.size
-    grayimg = Image.new("RGB", [width, height], (16*8, 20*8, 16*8))
+    grayimg = Image.new("RGB", (width, height), (16*8, 20*8, 16*8))
     # Change color
     for x in range(width):
         for y in range(height):
-            current_color = newimg.getpixel((x,y))
+            current_color = newimg.getpixel((x, y))
             try:
-                new_color = list(PaletteDict[current_color]) 
+                new_color = PaletteDict[current_color]
             except KeyError:
+                print(current_color)
                 new_color = current_color
-            grayimg.putpixel((x,y), tuple(new_color))
+            grayimg.putpixel((x, y), new_color)
 
     return grayimg
 
 for fp in os.listdir('.'):
-  if fp.endswith('.png'):
-    picture = Image.open(fp)
+    if fp.endswith('.png'):
+        print(fp)
+        picture = Image.open(fp)
 
-    # Get the size of the image
-    width, height = picture.size
+        # Get the size of the image
+        width, height = picture.size
 
-    # Get only the passive_sprites
-    passive_sprites = picture.crop((0, 0, width, height/3))
-    gray_passive_sprites = grayconvert(passive_sprites)
-    # Paste the new gray versions of the passive sprites 1/3 of the way down
-    picture.paste(gray_passive_sprites, (0, height/3))
+        # Get only the passive_sprites
+        passive_sprites = picture.crop((0, 0, width, height/3))
+        gray_passive_sprites = grayconvert(passive_sprites)
+        # Paste the new gray versions of the passive sprites 1/3 of the way down
+        picture.paste(gray_passive_sprites, (0, height/3))
 
-    picture.save('Fixed'+fp)
+        picture.save('Fixed_' +fp)
 
-print "Done"
-
+print("Done")
