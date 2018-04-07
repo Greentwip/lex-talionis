6e764193443726cf55aab8444cac49b9411cbffe
rainlash <rainlash>
2018-04-06 21:26:26 -0500

    Added Thief

--- Utilities/Animations/palette_converter.py
+++ Utilities/Animations/palette_converter.py
@@ -3,8 +3,8 @@ import glob
 from palette_index import *
 
 ### === INPUTS ===
-p1 = swordmaster_blue
-p2 = razia
+p1 = thief_cath
+p2 = thief_renae
 images_to_convert = '*.png'
 
 def palette_convert(p1, p2, image_to_convert):
