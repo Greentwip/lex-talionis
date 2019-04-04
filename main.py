6bf1d6b98362e0fb1552f21110cbc044eb837075
rainlash <rain.pipe.ash@gmail.com>
2019-04-04 11:45:53 -0500

    Update to logging and loading configuration file

--- main.py
+++ main.py
@@ -112,7 +112,8 @@ if __name__ == '__main__':
     else:
         my_level = logging.WARNING
     logging.basicConfig(filename='./Saves/debug.log.1', filemode='w',
-                        level=my_level, format='%(levelname)8s:%(module)20s: %(message)s')
+                        level=my_level, format='%(relativeCreated)d %(levelname)8s:%(module)20s: %(message)s')
+    logger.info('*** Lex Talionis Engine Version %s ***' % GC.version)
     try:
         main()
     except Exception as e:
