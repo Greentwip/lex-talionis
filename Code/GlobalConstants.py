baefa93242ed3c56118de37a53544cf4ce4fed7b
rainlash <rainlash>
2020-01-03 20:18:55 -0600

    version number

--- Code/GlobalConstants.py
+++ Code/GlobalConstants.py
@@ -7,7 +7,7 @@ from . import configuration as cf
 import logging
 logger = logging.getLogger(__name__)
 
-version = "0.9.3.10"
+version = "0.9.3.11"
 # === GLOBAL CONSTANTS ===========================================
 FPS = 60
 FRAMERATE = 1000//FPS
