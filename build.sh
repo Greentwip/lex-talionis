98415f8b20ae6114121ceaf75ff5f6b2f1a16b11
Greentwip <vjlopezcarrillo@gmail.com>
2021-02-17 20:59:07 -0700

    Correct sensor setting

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 export ANDROIDAPI="26"
-p4a apk --private . --package=com.greentwip.lextalionis --name "Lex Talionis" --version 0.1 --bootstrap=sdl2 --requirements=python3,pygame,kivy --sdk-dir ~/Android/Sdk --ndk-dir ~/Android/ndk/r19c --orientation landscapeSensor --release
+p4a apk --private . --package=com.greentwip.lextalionis --name "Lex Talionis" --version 0.1 --bootstrap=sdl2 --requirements=python3,pygame,kivy --sdk-dir ~/Android/Sdk --ndk-dir ~/Android/ndk/r19c --orientation sensorLandscape
